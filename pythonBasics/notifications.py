fa41c2d7924df8bbd6a3ccfc006cfbd3e3f5c828
GoodGuyGregory <greg.witt625@gmail.com>
2018-12-20 23:22:52 -0500

    adds progress to notifications

--- pythonBasics/notifications.py
+++ pythonBasics/notifications.py
@@ -1,4 +1,5 @@
 # fine looking unsmelly code 
+
 praise = "You are doing great"
 #strings are immutable therefore we use the method upper to capitalize
 praise = praise.upper()
@@ -19,4 +20,14 @@ number_of_characters = len(advice2)
 result = advice2 + "!" * number_of_characters
 print(result)
 
+#Function to do this
+def yell(text):
+	text = text.upper()
+	number_of_characters = len(text)
+	result = text + "!" * (number_of_characters // 2)
+	print(result)
+
+yell("You are doing great")
+yell("Don't repeat yourself. Keep it DRY")
+yell("Don't forget to ask for help")
 
